b8bfe90e23d7283d42555407d5e8bd93d5d55f46
yangjian <809127232@qq.com>
2018-12-06 22:13:18 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+// 初始化的css
+import './assets/style/reset.css'
+// 解决1像素边框的css
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+// 解决点击事件延迟300毫秒的问题
+fastClick.attach(document.body)
 
-/* eslint-disable no-new */
+/* eslint-disable no-new  项目的根文件,这是一个根实例 */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同，返回不同的内容给用户
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  hello
+  </div>
+</template>
+
+<script>
+export default {
+  // 组件的名字
+  name: 'home'
+}
+</script>
+
+<style scoped>
+
+</style>
