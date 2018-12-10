69d4825a8e0015db6e8a2d7e8db009dbceca3fc5
yangjian <809127232@qq.com>
2018-12-10 19:14:33 +0800

    add city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
     <span class="iconfont">&#xe632;</span>
     输入城市/景点/游玩主题
   </div>
-  <div class="header-right">
-    {{ this.city }}
-    <span class="iconfont arrow-icon">&#xe64a;</span>
-  </div>
+  <!-- router-link 在vue中用来替代a标签  -->
+  <router-link to="/city">
+    <div class="header-right">
+      {{ this.city }}
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </router-link>
 </div>
 </template>
 
@@ -28,7 +31,7 @@ export default {
   @import "~styles/varibles.styl"
 .header
   display flex
-  line-height: .86rem
+  line-height: $headerHeight
   background: $bgColor
   color: #fff
   .header-left
@@ -51,6 +54,7 @@ export default {
     width: 1.24rem
     float: right
     text-align center
+    color #fff
     .arrow-icon
       font-size .24rem
       margin-left -.04rem
